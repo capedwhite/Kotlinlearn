06ff224f9df21adf5286d002403a59305794cb24
smarika <mesmarika7@gmail.com>
2025-11-03 13:59:29 +0545

    first commit with arrays

---
+++ app/src/main/java/com/example/a37apractice/learning/Arrays.kt
@@ -0,0 +1,14 @@
+package com.example.a37apractice.learning
+
+// using array of
+fun main(args:Array<String>){
+var age = arrayOf(2,3,4,5);
+    println(age);
+    println("the first value is "+age[1]);
+    println("the second value is "+age[2]);
+    println("the third value is "+age[3]);
+    println("*****************************");
+    var name = arrayOf("hari","ram");
+    println("the first name is ${name[0]} and second name is ${name[1]}");
+            println(age.size);
+}
